2ad42e7a8e4986e00632056b73335ddd9439d4fc
Alex Conrey <alex.conrey@gigenet.com>
2014-12-11 21:01:32 -0600

    trying to mess around with javascript

--- application/config/database.php
+++ application/config/database.php
@@ -49,7 +49,7 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
+$db['default']['username'] = 'dashome';
 $db['default']['password'] = 'passw0rd';
 $db['default']['database'] = 'dashome';
 $db['default']['dbdriver'] = 'mysql';
--- application/views/wol.php
+++ application/views/wol.php
@@ -9,7 +9,15 @@ include('header.php');
   <div class="page-header">
     <h1>Wake up Computers</h1>
     <hr>
-    <a href="#">Add Computer</a>
+<!--<a href="#" id="addScnt">Add Device</a>-->
+<a href="#" id="addHost">Add Device</a>
+
+<div id="p_scents">
+    <p>
+        <!--<label for="p_scnts"><input type="text" id="p_scnt" size="20" name="p_scnt" value="" placeholder="Input Value" /></label>-->
+    </p>
+</div>
+
   </div>
   <div class="row">
   </div>
@@ -39,6 +47,28 @@ include('header.php');
 </body>
 
 <script>
+$(function() {
+        var scntDiv = $('#p_scents');
+        var i = $('#p_scents p').size() + 1;
+        $('#addHost').click(function() {
+            $(scntDiv).show("slow", function() {
+              //complete
+            });
+        });
+        $('#addScnt').live('click', function() {
+                $('<p><label for="p_scnts"><input type="text" id="p_scnt" size="20" name="p_scnt_' + i +'" value="" placeholder="Input Value" /></label> <a href="#" id="remScnt">Remove</a></p>').appendTo(scntDiv);
+                i++;
+                return false;
+        });
+        
+        $('#remScnt').live('click', function() { 
+                if( i > 2 ) {
+                        $(this).parents('p').remove();
+                        i--;
+                }
+                return false;
+        });
+});
 
 </script>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
